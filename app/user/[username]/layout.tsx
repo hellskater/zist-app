da21b09a5deb884b6d43fcffe42b80e71fbf5131
K Srinivas Rao <ksrinivasrao531@gmail.com>
2023-08-26 16:07:05 +0530

    dynamic user

--- app/user/[username]/layout.tsx
+++ app/user/[username]/layout.tsx
@@ -1,10 +1,39 @@
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 
 import UserProfile from '@/components/user/user-profile';
 
-export const metadata: Metadata = {
-  title: 'User | Zist',
-};
+export async function generateMetadata(
+  { params }: { params: { username: string } },
+  parent?: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const username = params.username;
+
+  // fetch data
+  const githubProfile = await fetch(
+    `https://api.github.com/users/${username}`
+  ).then((res) => res.json());
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent)?.openGraph?.images || [];
+
+  return {
+    title: `${githubProfile?.name || githubProfile?.login || 'User'} | Zist`,
+    description: githubProfile?.bio,
+    openGraph: {
+      title: `${githubProfile?.name || githubProfile?.login || 'User'} | Zist`,
+      description: githubProfile?.bio,
+      url: `https://zistapp.xyz/user/${username}`,
+      images: [githubProfile?.avatar_url, ...previousImages],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${githubProfile?.name || githubProfile?.login || 'User'} | Zist`,
+      description: githubProfile?.bio,
+      images: [githubProfile?.avatar_url, ...previousImages],
+    },
+  };
+}
 
 export default async function UserLayout({
   children,
