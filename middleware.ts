65255370e6bf6e931f5762c7e126da438316c503
K Srinivas Rao <ksrinivasrao531@gmail.com>
2023-08-26 21:44:11 +0530

    bug fixes

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get('next-auth.session-token');
+  const token =
+    request.cookies.get('next-auth.session-token') ||
+    request.cookies.get('__Secure-next-auth.session-token');
 
   if (!token) {
     return NextResponse.redirect(new URL('/', request.url));
